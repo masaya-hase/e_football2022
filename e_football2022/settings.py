6f59a36c0805bd99b027f17cff7c6491ec3f7406
masaya.hase <messi0124578@gmail.com>
2022-08-15 15:28:34 +0900

    chenge3

--- e_football2022/settings.py
+++ e_football2022/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'e_football2022.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default':{ 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-        }
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'admindatabase',
+    'USER': 'admin',
+    'PASSWORD': 'adminpassword',
+    'HOST': '',
+    'PORT': '',
+    }
 }
 
 
